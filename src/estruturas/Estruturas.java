58f49aa58fcc0e0ae3954479ee84fce26d347ee1
Fernando Cassimiro de Macedo <fernandocassimiro@uol.com.br>
2018-07-26 10:09:36 -0300

    Java_aula_Estruturas

---
+++ src/estruturas/Estruturas.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estruturas;
+
+/**
+ *
+ * @author Fernando
+ */
+public class Estruturas {
+
+    /**
+     * @param args the command line arguments
+     */
+    @SuppressWarnings("empty-statement")
+    /*public static void main(String[] args) {
+     for (int i = 0; i < 10; i++ ) {
+         System.out.println ("item é:" + i);
+                 }
+    }*/
+    public static void main(String[] args) {              
+        int k = 0;              
+        while(k<5){                  
+            System.out.println("Olá mundo!");                  
+            k++;             
+        }         
+    }
+}
